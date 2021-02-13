9fd2e7dbfc2d02f091647d413153b50bd53fc37d
Capelan <39345286+Capelan@users.noreply.github.com>
2021-02-13 13:00:42 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en">
+	<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<title>Hello, world!</title>
+
+	<!-- Favicon ico -->
+	<link rel="shortcut icon" href="resources/images/favicon.ico" type="image/x-icon">
+
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="resources/css/bootstrap.min.css">
+
+	<!-- Your custom styles (optional) -->
+	<link href="resources/css/main.css" rel="stylesheet">
+	<link href="resources/css/fonts.css" rel="stylesheet">
+	<link href="resources/css/media.css" rel="stylesheet">
+
+	<!-- FontAwesome -->
+	<script src="https://kit.fontawesome.com/c2e646cdf6.js" crossorigin="anonymous"></script>
+
+    <!-- Scripts js other -->
+    <script src="resources/js/data.js"></script>
+    <script src="resources/js/lang.js"></script>
+</head>
+
+<body>
+	<div class="flex-cont p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+		<div class="logo">
+			<a class="navbar-brand" href="#">
+				<img src="resources/images/logo.png" class="img-responsive" style="width: 50%;">
+			</a>
+		</div>
+
+		<h5 class="my-0 mr-md-auto font-weight-normal">OrionUO launcher</h5>
+
+		<!--<nav class="my-2 my-md-0 mr-md-3">
+			<a class="p-2 text-dark" href="#">Support</a>
+			<a class="p-2 text-dark" href="#">Wiki</a>
+		</nav>
+
+		<a class="btn btn-outline-primary" href="#">Download</a>-->
+	</div>
+
+<br /><br /><br /><br /><br />
+
+	<section id="main">
+		<div class="container mt-5 mb-5">
+			<div class="col-md-12">
+				<div class="row">
+					<div class="col-md-6 text-center">
+						<div class="card mb-4 box-shadow">
+							<div class="card-header">
+								<h4 class="my-0">Windows <span style="font-size: 22px;" tooltip="Минимальная версия OS - Win 7 SP2">*</span></h4>
+							</div>
+
+							<div class="card-body">
+								<p>
+									<button type="button" class="btn btn-lg btn-primary" style="width: 40%">
+										<a href="http://orionuo.online/Updates/OrionLauncher/x32/Orion%20Launcher_2.0.0.0.exe" style="color: white; text-decoration: none;">
+											Download x32
+										</a>
+									</button>
+									<button type="button" class="btn btn-lg btn-primary" style="width: 40%">
+										<a href="http://orionuo.online/Updates/OrionLauncher/x64/Orion%20Launcher64_2.0.0.0.exe" style="color: white; text-decoration: none;">
+											Download x64
+										</a>
+									</button>
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-6 text-center">
+						<div class="card mb-4 box-shadow">
+							<div class="card-header">
+								<h4 class="my-0">Linux <span style="font-size: 22px;" tooltip="Tested on Mint 19.3 To start, go to the terminal in the folder where the launcher was unpacked and run it.">*</span></h4>
+							</div>
+
+							<div class="card-body">
+								<p>
+									<button type="button" class="btn btn-lg btn-primary" style="width: 100%">
+										<a href="http://orionuo.online/Updates/OrionLauncher/linux64/OrionLauncher64.zip" style="color: white; text-decoration: none;">
+											Download x64
+										</a>
+									</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-12">
+				<div class="row">
+					<div class="col-md-6 text-center  mx-auto">
+						<div class="card mb-4 box-shadow">
+							<div class="card-header">
+								<h4 class="my-0">Other <span style="font-size: 22px;" tooltip="(patched for Orion, unpack in to UOAM directory)">*</span></h4>
+							</div>
+
+							<div class="card-body">
+								<p>
+									<button type="button" class="btn btn-lg btn-primary" style="width: 100%">
+										<a href="http://orionuo.online/Updates/Tools/uoam_Orion.zip" style="color: white; text-decoration: none;">
+											Old UOAM
+										</a>
+									</button>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="footer">
+		<footer class="pt-4 my-md-5 pt-md-5 border-top fixed-bottom">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md text-center">
+						<span class="text-muted align-middle">© Copyright <script type="text/javascript">document.write(today);</script> - OrionUO</span>
+						<br />
+						<span class="text-muted">Все права защищены.</span>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</section>
+
+    <!-- Scripts js -->
+    <script src="resources/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="resources/js/popper.min.js"></script>
+    <script src="resources/js/bootstrap.min.js"></script>
+  </body>
+</html>
